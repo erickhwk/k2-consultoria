96c126653342466fee0d4f90d0b9d4a7806aa2c5
Erick Valente <erickhwk@gmail.com>
2024-06-23 14:56:25 -0300

    add tests for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
   belongs_to :law_firm, optional: true
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         belongs_to :law_firm, optional: true
-         
-  enum role: { admin: 'admin', lawyer: 'lawyer' }
-  
+
+  enum role: { admin: 'admin', manager: 'manager' }
+
+  validates :email, presence: true, email: true
+  validates :password, presence: true
   validates :role, presence: true, inclusion: { in: roles.keys }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it 'requires email to be present' do
+      user = User.new(email: nil, password: 'password', role: 'admin')
+      expect(user).not_to be_valid
+      expect(user.errors[:email]).to include("can't be blank")
+    end
+
+    it 'requires password to be present' do
+      user = User.new(email: 'test@example.com', password: nil, role: 'admin')
+      expect(user).not_to be_valid
+      expect(user.errors[:password]).to include("can't be blank")
+    end
+
+    it 'requires role to be either admin or manager' do
+      expect {
+        User.new(email: 'test@example.com', password: 'password', role: 'invalid_role')
+      }.to raise_error(ArgumentError, "'invalid_role' is not a valid role")
+    end
+  end
+
+  describe 'associations' do
+    it 'should belong to law_firm' do
+      association = described_class.reflect_on_association(:law_firm)
+      expect(association.macro).to eq :belongs_to
+    end
+  end
+
+  describe 'behaviors' do
+    it 'creates a valid user' do
+      user = User.new(email: 'test@example.com', password: 'password', role: 'admin')
+      expect(user).to be_valid
+    end
+
+    it 'is associated with a law firm' do
+      law_firm = LawFirm.create(name: 'Example Law Firm', cnpj: '12345678901234')
+      user = User.new(email: 'test@example.com', password: 'password', role: 'manager', law_firm: law_firm)
+      expect(user.law_firm).to eq(law_firm)
+    end
+  end
 end
